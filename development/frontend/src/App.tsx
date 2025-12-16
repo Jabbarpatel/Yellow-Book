436f41f57734e85b0a8286d0bdd74deb6b64ce2c
Jabbar <j.patel@bm-systems.in>
2025-12-16 14:07:40 +0200

    removed unused code

--- development/frontend/src/App.tsx
+++ development/frontend/src/App.tsx
@@ -1,23 +1,8 @@
 import "./App.css";
-import { Button } from "@heroui/react";
 import Providers from "./components/Providers/Providers";
-import ReactModal from "./components/General/ReactModal";
-import { useState } from "react";
 
 function App() {
-  const [test, setTest] = useState(false);
-  const close = () => {
-    setTest(false);
-  };
-
-  return (
-    <Providers>
-      <Button onPress={() => setTest(true)}>CLick</Button>
-      <ReactModal isOpen={test} title="Test" size="2xl" onClose={close}>
-        Thisi sisisiisisi
-      </ReactModal>
-    </Providers>
-  );
+  return <Providers>THIS IS OK</Providers>;
 }
 
 export default App;
